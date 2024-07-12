ea1fdd0ec6c9d44035b17dd8fba63ef08a8d4927
nya~ <ara@0ms.dev>
2024-07-12 14:21:47 +0700

    added initial support for db transactions

--- handlers/routes.go
+++ handlers/routes.go
@@ -6,10 +6,12 @@ func (server *Server) home() {
 
 func (server *Server) login() {
 	server.Router.GET("/login", RenderLoginPage)
+	server.Router.POST("/login", RenderLoginPage) // Add POST method for login
 }
 
 func (server *Server) register() {
 	server.Router.GET("/register", RenderRegisterPage)
+	server.Router.POST("/register", RenderRegisterPage) // Add POST method for register
 }
 
 func (server *Server) apiStatus() {
