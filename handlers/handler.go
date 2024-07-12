c1815a9282f910fc46b49dc5d96f1b0b9ec37552
nya~ <ara@0ms.dev>
2024-07-12 16:30:24 +0700

    test writeToDB()

--- handlers/handler.go
+++ handlers/handler.go
@@ -20,6 +20,34 @@ func RenderRegisterPage(c *gin.Context) {
 	c.HTML(200, "register.html", gin.H{})
 }
 
+func HandleRegistrasi(c *gin.Context) {
+	var (
+		acc Account
+	)
+
+	c.Bind(&acc)
+
+	// tes print data dari form ke console
+	templatePrint := fmt.Sprintf("Nama: %v\nEmail: %v\nUsername: %v\nPassword: %v\nNoHandphone: %v\n", acc.Nama, acc.Email, acc.Username, acc.Password, acc.NoHandphone)
+	fmt.Println("--- Test Registrasi ---")
+	fmt.Println(templatePrint)
+	fmt.Println("--- --- ---")
+
+	// tambah data ke DB
+	writeToDB(fmt.Sprintf("acc.user.%v.pass", acc.Username), acc.Password)
+	writeToDB(fmt.Sprintf("acc.user.%v.nama", acc.Username), acc.Nama)
+	writeToDB(fmt.Sprintf("acc.user.%v.email", acc.Username), acc.Email)
+	writeToDB(fmt.Sprintf("acc.user.%v.nohp", acc.Username), fmt.Sprintf("%v", acc.NoHandphone))
+
+	c.JSON(200, gin.H{
+		"Nama":        acc.Nama,
+		"Email":       acc.Email,
+		"Username":    acc.Username,
+		"Password":    acc.Password,
+		"NoHandphone": acc.NoHandphone,
+	})
+}
+
 func ApiStatus(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": fmt.Sprintf("Server time: %v", time.Now().UTC().Format(time.RFC850)),
--- handlers/routes.go
+++ handlers/routes.go
@@ -1,17 +1,27 @@
 package handlers
 
 func (server *Server) home() {
+	// ini untuk munculkan halaman homepage
 	server.Router.GET("/", RenderHome)
 }
 
 func (server *Server) login() {
+	// ini untuk munculkan halaman login
 	server.Router.GET("/login", RenderLoginPage)
+
+	// ini untuk handle login ketika submit form dengan method POST
+	server.Router.POST("/login", RenderLoginPage)
 }
 
 func (server *Server) register() {
+	// ini untuk munculkan halaman registrasi
 	server.Router.GET("/register", RenderRegisterPage)
+
+	// ini untuk handle registrasi ketika submit form dengan method POST
+	server.Router.POST("/register", HandleRegistrasi)
 }
 
 func (server *Server) apiStatus() {
+	// ini untuk munculkan status API
 	server.Router.GET("/api", ApiStatus)
 }
